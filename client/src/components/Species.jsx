1869e15652f6d0bcf1c6bbbafcc5df92bbf9f86a
Janet Jiang <janetjiang1103@gmail.com>
2023-09-20 09:37:35 +0800

    Modifing card to represent species

--- client/src/components/Species.jsx
+++ client/src/components/Species.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import moment from 'moment';
 import Button from 'react-bootstrap/Button';
 import * as ioicons from 'react-icons/io5'
 
@@ -17,7 +18,12 @@ const Species = ({species}) => {
         <Card>
             <Card.Body>
             <Card.Title>{species.commonname} </Card.Title>
-            <Card.Subtitle>{species.scientificname}</Card.Subtitle>
+            <Card.Subtitle>Scientific Name: {species.scientificname}</Card.Subtitle>
+            <Card.Text>
+            <div>Number in Wild: {species.numinwild}</div>
+            <div>Code: {species.cscode}</div>
+            <div>Record created at: {moment(species.createat).format('LLL')}</div>
+          </Card.Text>
             {/* <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button> */}
             </Card.Body>
